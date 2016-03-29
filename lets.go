ce2696e4d94344d7317aadda639cbf0ce83b2fd7
spalman <spalman01@gmail.com>
2016-03-29 19:10:10 +0300

    think it works correct

--- lets.go
+++ lets.go
@@ -80,12 +80,99 @@ func addRelations(upperEdge, lowerEdge, relType int) {
 	}
 }
 
-func requestHandler() {
+func requestHandler(request string) {
+	temp := strings.Split(request, ":")
+	//fmt.Printf("temp: %s %s %s", temp[0], temp[1], temp[2])
+	temp[2] = temp[2][0 : len(temp[2])-1]
 
+	switch {
+	// True/false request
+	case temp[0] != "?" && temp[1] != "?" && temp[2] != "?":
+		i0, _ := strconv.Atoi(temp[0])
+		i1, _ := strconv.Atoi(temp[1])
+		i2, _ := strconv.Atoi(temp[2])
+		if adjacencyMatrix[i0][i2] == i1 {
+			fmt.Printf("\nTrue")
+		} else {
+			fmt.Printf("\nFalse")
+		}
+
+		// First param unknown
+		//Shows objects that has selected relation with another one
+	case temp[0] == "?" && temp[1] != "?" && temp[2] != "?":
+		i1, _ := strconv.Atoi(temp[1])
+		i2, _ := strconv.Atoi(temp[2])
+		for i := range adjacencyMatrix {
+			if adjacencyMatrix[i][i2] == i1 {
+				fmt.Printf("%s %s %s\n", baseTerms[i], baseRelations[i1].name, baseTerms[i2])
+			}
+		}
+
+		//Fist & second unknown
+		//Finds relations with second object
+	case temp[0] == "?" && temp[1] == "?" && temp[2] != "?":
+		i2, _ := strconv.Atoi(temp[2])
+		for i := range adjacencyMatrix {
+			if adjacencyMatrix[i][i2] != 0 {
+				fmt.Printf("%s %s %s\n", baseTerms[i], baseRelations[adjacencyMatrix[i][i2]].name, baseTerms[i2])
+			}
+		}
+
+		//Second & third param unknown
+	case temp[0] != "?" && temp[1] == "?" && temp[2] == "?":
+		i0, _ := strconv.Atoi(temp[0])
+		for i := range adjacencyMatrix[i0] {
+			if adjacencyMatrix[i0][i] != 0 {
+				fmt.Printf("%s %s %s\n", baseTerms[i0], baseRelations[adjacencyMatrix[i0][i]].name, baseTerms[i])
+			}
+		}
+
+		//Second param unknown
+		//Shows relation between two objects
+	case temp[0] != "?" && temp[1] == "?" && temp[2] != "?":
+		i0, _ := strconv.Atoi(temp[0])
+		i2, _ := strconv.Atoi(temp[2])
+		if adjacencyMatrix[i0][i2] != 0 {
+			fmt.Printf("%s %s %s\n", baseTerms[i0], baseRelations[adjacencyMatrix[i0][i2]].name, baseTerms[i2])
+
+		} else {
+			fmt.Printf("No relations between this objects\n")
+		}
+	//Third param unknown
+	//Finds relations with first object
+	case temp[0] != "?" && temp[1] != "?" && temp[2] == "?":
+		i0, _ := strconv.Atoi(temp[0])
+		i1, _ := strconv.Atoi(temp[1])
+		for i := range adjacencyMatrix[i0] {
+			if adjacencyMatrix[i0][i] == i1 {
+				fmt.Printf("%s %s %s\n", baseTerms[i0], baseRelations[i1].name, baseTerms[i])
+			}
+		}
+	//First && third unknown
+	case temp[0] == "?" && temp[1] != "?" && temp[2] == "?":
+		i1, _ := strconv.Atoi(temp[1])
+		for i := range adjacencyMatrix {
+			for j := range adjacencyMatrix {
+				if adjacencyMatrix[i][j] == i1 {
+					fmt.Printf("%s %s %s\n", baseTerms[i], baseRelations[adjacencyMatrix[i][j]].name, baseTerms[j])
+				}
+			}
+		}
+	//All params "?"
+	//Print all data
+	case temp[0] == "?" && temp[1] == "?" && temp[2] == "?":
+		for i := range adjacencyMatrix {
+			for j := range adjacencyMatrix[i] {
+				if adjacencyMatrix[i][j] != 0 {
+					fmt.Printf("%s %s %s\n", baseTerms[i], baseRelations[adjacencyMatrix[i][j]].name, baseTerms[j])
+				}
+			}
+		}
+	}
 }
 
 func main() {
-	parseTxt("1.txt")
+	parseTxt("TestData/1.txt")
 	process()
 	for i := range adjacencyMatrix {
 		b := false
@@ -99,5 +186,9 @@ func main() {
 			fmt.Printf("\n")
 		}
 	}
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	requestHandler(text)
 
 }
